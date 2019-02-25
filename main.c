53fc743740bee86373376165620e1f9cf4086a2f
YIIIIIACH <1997marc212@gmail.com>
2019-02-25 23:39:07 +0800

    2019/02/25 finish

--- main.c
+++ main.c
@@ -5,19 +5,31 @@ int plus(int a, int b) { return a + b; }
 int minus(int a, int b) { return a - b; }
 int multiply(int a, int b) { return a * b; }
 int divided(int a, int b) { return a / b; }
+static int (*operator[])(int,int)={ plus , minus , multiply , divided};
 int main()
 {
-	int a, c;
+	int a, c, op;
 	char b;
 	printf("key a Function \nEX: 1 + 1\n");
 	scanf("%i %c %i", &a, &b, &c);
-	//IF ELSE IF
-	if (b == '+')
-		printf("%d %c %d = %d\n", a, b, c, plus(a, c));
-	else if (b == '-')
-		printf("%d %c %d = %d\n", a, b, c, minus(a, c));
-	else if (b == '*')
-		printf("%d %c %d = %d\n", a, b, c, multiply(a, c));
-	else if (b == '/')
-		printf("%d %c %d = %d\n", a, b, c, divided(a, c));
+	switch (b)
+	{
+		case '+':
+			op=0;
+			break;
+		case '-':
+			op=1;
+			break;
+		case '*':
+			op=2;
+			break;
+		case '/':
+			op=3;
+			break;
+		default:
+			break;
+	}	
+	a=operator[op](a,c);
+	printf("Answer is %i\n", a);
+	return 0;
 }
